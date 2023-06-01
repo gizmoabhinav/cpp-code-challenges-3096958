f8276c159e4e1a4f2651b685fd80d4b7eec8619c
Abhinav Mukherjee <gizmoabhinav@users.noreply.github.com>
2023-06-01 09:03:53 +0000

    ch 11 solution

--- src/11/Challenge/ch11_relatives.cpp
+++ src/11/Challenge/ch11_relatives.cpp
@@ -10,16 +10,38 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include <sstream>
 
 // Finding Relatives, main()
 // Summary: This application displays the first pair of possible relatives from a list of names in a CSV file.
 int main(){
+    std::vector<std::vector<std::string>> people;
+    std::vector<std::string> person;
+    std::string entry, name;
+    std::unordered_map<std::string, std::vector<std::string>> family;
     std::fstream file ("names.csv", std::ios::in);
     // Read the CSV file.
     if(file.is_open()){
-
-        // Write your code here
+        while (std::getline(file,entry,',')) {
+            std::stringstream str(entry);
+            person.clear();
+            while (std::getline(str,name,' ')) {
+                if (person.size() != 0) {
+                    if (family.find(name) == family.end()) {
+                        person.push_back(name);
+                        family[name] = person;
+                    } else {
+                        std::cout << family[name][0] << " " << family[name][1] << " is related to " << person[0] << " " << person[1] << std::endl;
+                        file.close();
+                        return 0;
+                    }
+                } else {
+                    person.push_back(name);
+                }
+            }
+            people.push_back(person);
+        }
     
         file.close();
     }
